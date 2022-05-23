204b2967cd79ff6b5f2f95c998e025560bff1f25
Mostafa Shamin Yeasar <mostafa.shaminbjit@gmail.com>
2022-05-23 17:39:00 +0600

    Initial Commit

---
+++ mapSwiftUI/mapSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapSwiftUIApp.swift
+//  mapSwiftUI
+//
+//  Created by BJIT on 23/5/22.
+//
+
+import SwiftUI
+
+@main
+struct mapSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
